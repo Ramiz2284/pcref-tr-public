f688ad0e9ada4371e5139509429b897ea95917ba
Ramiz2284 <sultan22.84@mail.ru>
2023-09-05 14:07:36 +0300

    regSlice alert add

--- src/Component/Redux/RegSlice.js
+++ src/Component/Redux/RegSlice.js
@@ -1,40 +1,43 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "./axios";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from './axios'
 
-export const fetchRegister = createAsyncThunk('auth/fetchRegister', async (params) => {
-    const { data } = await axios.post('/auth/register', params);
-    return data;
-});
+export const fetchRegister = createAsyncThunk(
+	'auth/fetchRegister',
+	async (params, { rejectWithValue }) => {
+		try {
+			const { data } = await axios.post('/auth/register', params)
+			return data
+		} catch (error) {
+			window.alert('Ошибка при регистрации: ' + error.message) // Добавьте эту строку для алерта
+			return rejectWithValue(error.message)
+		}
+	}
+)
 
 const initialState = {
-    data: null,
-    status: 'loading',
-};
-
+	data: null,
+	status: 'loading',
+}
 
 const RegSlice = createSlice({
-    name: 'registr',
-    initialState,
-    extraReducers: {
-        [fetchRegister.pending]: (state) => {
-            state.status = 'loading';
-            state.data = null;
-        },
-        [fetchRegister.fulfilled]: (state, action) => {
-            state.status = 'loaded';
-            state.data = action.payload;
-        },
-        [fetchRegister.rejected]: (state) => {
-            state.status = 'error';
-            state.data = null;
-        },
-    }
-});
+	name: 'registr',
+	initialState,
+	extraReducers: {
+		[fetchRegister.pending]: state => {
+			state.status = 'loading'
+			state.data = null
+		},
+		[fetchRegister.fulfilled]: (state, action) => {
+			state.status = 'loaded'
+			state.data = action.payload
+		},
+		[fetchRegister.rejected]: state => {
+			state.status = 'error'
+			state.data = null
+		},
+	},
+})
 
-export const selectIsRegister = (state) => Boolean(state.reg.data);
+export const selectIsRegister = state => Boolean(state.reg.data)
 
-export const RegistrReducer = RegSlice.reducer;
-
-
-
-
+export const RegistrReducer = RegSlice.reducer
--- src/Component/Redux/axios.js
+++ src/Component/Redux/axios.js
@@ -1,16 +1,14 @@
-
-import axios from 'axios';
+import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: process.env.REACT_APP_DO_URL   // url для heroku
-    baseURL: 'https://pcref.site:9000'
-    // baseURL: 'http://localhost:4444'
-});
+	// baseURL: process.env.REACT_APP_DO_URL   // url для heroku
+	baseURL: 'https://pcref.site:9000',
+	// baseURL: 'http://localhost:4444'
+})
 
+instance.interceptors.request.use(config => {
+	config.headers.Authorization = window.localStorage.getItem('token')
+	return config
+})
 
-instance.interceptors.request.use((config) => {
-    config.headers.Authorization = window.localStorage.getItem('token');
-    return config;
-});
-
-export default instance;
+export default instance
